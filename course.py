a4793a8977fb78fbecf51c8413b7767e036bfc91
A-Hlongwana01 <74430184+A-Hlongwana01@users.noreply.github.com>
2021-10-10 23:57:42 +0200

    Add files via upload

---
+++ course.py
@@ -0,0 +1,14 @@
+
+
+def create_outline():
+    """
+    TODO: implement your code here
+    """
+    pass
+    # Step 1: 'set' of Topics
+    topics = {"Introduction to Python","Tools of the Trade","How to make decisions","How to repeat code","How to structure data","Functions","Modules"}
+    print("Course Topics:")
+    for i in topics:
+        print("*"+i)
+if __name__ == "__main__":
+    create_outline()
